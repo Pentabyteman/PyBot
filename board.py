6d6bf91afad2f0cd65084048626e0ce7711740f2
erichhasl <erich.hasl@gmail.com>
2016-10-10 16:24:54 +0200

    Added robot class and movement

--- board.py
+++ board.py
@@ -1,33 +1,66 @@
 import pygame
+import bot_exceptions
+import robot
 import sprite
 
 TYPE_FIELD, TYPE_WALL = 0, 1
 
 
-class Board():
+class Map:
 
-    def __init__(self, size, shape=(9, 9)):
+    def __init__(self, fields):
+        self.fields = fields
+
+    def move(self, bot, row, col):
+        print("moving", bot, "to", row, col)
+        try:
+            if self.get(bot.row, bot.col) == bot:
+                self.set(None, bot.row, bot.col)  # clear old field
+        except AttributeError:  # when not initialized, in the beginning
+            pass
+        self.set(bot, row, col)  # move to new
+
+    def get(self, row, col):
+        return self.fields[row][col]
+
+    def set(self, bot, row, col):
+        field = self.fields[row][col]
+        if not field.passable and bot is not None:
+            raise bot_exceptions.IllegalMoveException("Field not passable")
+        field.entity = bot
+
+
+class Board:
+
+    def __init__(self, size, shape=(9, 9), robot_count=2):
         self.shape = shape  # default 9 by 9
         print("shape", self.shape)
         self.size = size  # actual size in pixels
 
         self.field_size = self.size[0] / 9, self.size[1] / 9
-        self.map = [[Field(self.field_size) for col in range(self.shape[1])]
-                    for col in range(self.shape[1])]
+        # generate map
+        self.map = Map([[Field(self.field_size)
+                        for col in range(self.shape[1])]
+                        for row in range(self.shape[0])])
+
+        # n robots
+        self.bots = [robot.Robot(self.field_size, team, self.map, (team,
+                                                                   team))
+                     for team in range(robot_count)]
 
     def draw(self):
         surf = pygame.Surface(self.size)
         surf.fill((255, 255, 255))
 
         # draw grid as background
-        for row, _ in enumerate(self.map):
+        for row, _ in enumerate(self.map.fields):
             for col, _ in enumerate(_):
                 w, h = self.field_size
                 rect = pygame.Rect(col * w, row * h, w, h)
                 pygame.draw.rect(surf, (100, 100, 100), rect, 1)
 
         # draw fields on top of it
-        for row, _ in enumerate(self.map):
+        for row, _ in enumerate(self.map.fields):
             for col, field in enumerate(_):
                 x, y = col * self.field_size[0], row * self.field_size[1]
                 surf.blit(field.image, (x, y))
@@ -36,18 +69,35 @@ class Board():
     def on_event(self, event=None):
         pass
 
+    def on_turn(self):
+        bot = self.next_bot()
+        bot.on_turn(self.map)
+
+    def next_bot(self):
+        idx = 0
+        while True:
+            yield self.bots[idx]
+            idx += 1
+
 
 class Field(sprite.Sprite):
 
-    def __init__(self, size, type=TYPE_FIELD, team=None):
+    def __init__(self, size, kind=TYPE_FIELD, team=None):
         super(Field, self).__init__(size)
-        self.type = type
+        self.kind = kind
+        self.entity = None
 
     def draw(self):
         self.surface.fill(self._bgcolor())
+        if self.entity:
+            self.surface.blit(self.entity.image, (0, 0))
+
+    @property
+    def passable(self):
+        return self.kind != TYPE_WALL and self.entity is None
 
     def _bgcolor(self):
-        if self.type == TYPE_FIELD:
+        if self.kind == TYPE_FIELD:
             return (0, 0, 0, 0)  # reset everything
-        elif self.type == TYPE_WALL:
+        elif self.kind == TYPE_WALL:
             return (150, 50, 50, 250)
---
+++ robot.py
@@ -0,0 +1,50 @@
+import pygame
+from bot_exceptions import IllegalMoveException
+import sprite
+
+
+class Robot(sprite.Sprite):
+
+    def __init__(self, size, team, map, pos=(0, 0)):
+        super(Robot, self).__init__(size)
+        self.team = team
+        self.map = map
+        try:
+            print("pos", pos)
+            self.pos = pos
+        except IllegalMoveException:
+            print("Not a valid position")
+
+    def draw(self):
+        pygame.draw.ellipse(self.surface, (255, 0, 0), self.surface.get_rect())
+
+    def on_turn(self):
+        pass
+
+    @property
+    def pos(self):
+        return (self.__row, self.__col)
+
+    @pos.setter
+    def pos(self, pos):
+        row, col = pos
+        self.map.move(self, *pos)
+        self.__row, self.__col = pos
+
+    @property
+    def row(self):
+        return self.__row
+
+    @row.setter
+    def row(self, row):
+        self.map.move(self, row, self.col)
+        self.__row = row
+
+    @property
+    def col(self):
+        return self.__col
+
+    @col.setter
+    def col(self, col):
+        self.map.move(self, col, self.row)
+        self.__col = col
