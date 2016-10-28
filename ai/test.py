057412edcf5d5adfc25911c8bd336b85f38d608e
erichhasl <erich.hasl@gmail.com>
2016-10-28 15:15:41 +0200

    fix bug, is now working for both robots

--- ai/test.py
+++ ai/test.py
@@ -6,19 +6,18 @@ def get_move(ask_for_field, turns_to_go, position=None, rotation=None):
     if position is not None and rotation is not None:
         pos = list(position)
         rot = rotation
+        own_team = ask_for_field(*pos)[1]
 
         for row in range(9):
             for col in range(9):
                 passable, team, is_entity = ask_for_field(row, col)
-                if is_entity:
+                if is_entity and team != own_team:
                     entity_pos = (row, col)
                     break
 
         rows_to_go, cols_to_go = [e - s for s, e in zip(pos, entity_pos)]
-        print("rows to go", rows_to_go)
         rows_to_go = rows_to_go + 1 if rows_to_go < 0 else rows_to_go - 1
         cols_to_go = cols_to_go + 1 if cols_to_go < 0 else cols_to_go - 1
-        print("rows to go", rows_to_go)
 
     # ACHTUNG GRAUSELIG! NICHT NACHMACHEN!
     if rows_to_go != 0:
@@ -34,6 +33,6 @@ def get_move(ask_for_field, turns_to_go, position=None, rotation=None):
             row_diff = min(1, max(-1, rows_to_go))
             pos[0] += row_diff
             rows_to_go = rows_to_go - 1 if rows_to_go > 0 else rows_to_go + 1
-            return "move {0}".format(row_diff)
+            return "move {0}".format(1)
 
     return "attack 0"
