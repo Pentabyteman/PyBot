1a7d134936a3c2dd77abb50c935b25aed4a72d49
Pentabyteman <nils@brennbach.de>
2016-11-07 18:05:29 +0100

    Update settings.py

--- client/settings.py
+++ client/settings.py
@@ -2,7 +2,7 @@
 # -*- coding: iso-8859-15 -*-
 
 import sysconfig
-
+import tkinter
 
 def get_pybot_platform():
     platform = str(sysconfig.get_platform())
@@ -27,50 +27,28 @@ def get_standard_settings():
             return settings_list
     except Exception as e:
         print("ERROR: Settings not readable. Forcing default settings.", e)
-        settings_list = ['true', 'true', "", "", "2.0"]
+        settings_list = ['none', "", "", "2.0"]
         return settings_list
 
 
 def update_standard_settings(user=None, host=None):
     try:
-        if get_standard_settings()[0] == 'true':
-            version = get_standard_settings()[3]
-            new_doc = "PROMPTING: true\nEDIT_ENABLED: true\nUSER: {0}\nHOST: {1}\nVERSION: {2}".format(user, host,
+        if user != get_standard_settings()[1] or host != get_standard_settings()[2]
+        if get_standard_settings()[0] == 'none':
+            if tkinter.messagebox.askyesno('Update Settings', 'Do you want to update your settings'):
+                version = get_standard_settings()[3]
+                new_doc = "PROMPTING: true\nEDIT_ENABLED: true\nUSER: {0}\nHOST: {1}\nVERSION: {2}".format(user, host,
                                                                                                        version)
-            with open('resources/settings.txt', 'w') as text:
-                text.seek(0)
-                text.write(new_doc)
-        else:
+                with open('resources/settings.txt', 'w') as text:
+                    text.seek(0)
+                    text.write(new_doc)
+                                                                                                           
+        elif get_standard_settings()[0] == 'always':
+            version = get_standard_settings()[3]
+                new_doc = "UPDATING: always\nUSER: {0}\nHOST: {1}\nVERSION: {2}".format(user, host, version)
+                with open('resources/settings.txt', 'w') as text:
+                    text.seek(0)
+                    text.write(new_doc)
             pass
-    except Exception as e:
-        print("FATAL ERROR: Settings not writable.", e)
-
-def disable_prompting():
-    try:
-        editable, user, host, version = get_standard_settings()
-        new_doc = "PROMPTING: false\nEDIT_ENABLED: true\nUSER: {0}\nHOST: {1}\nVERSION: {2}".format(user, host, version)
-        with open('resources/settings.txt', 'w') as text:
-            text.seek(0)
-            text.write(new_doc)
-    except Exception as e:
-        print("FATAL ERROR: Settings not writable.", e)
-
-def disable_edits():
-    try:
-        editable, user, host, version = get_standard_settings()
-        new_doc = "EDIT_ENABLED: false\nUSER: {0}\nHOST: {1}\nVERSION: {2}".format(user, host, version)
-        with open('resources/settings.txt', 'w') as text:
-            text.seek(0)
-            text.write(new_doc)
-    except Exception as e:
-        print("FATAL ERROR: Settings not writable.", e)
-
-def enable_edits():
-    try:
-        editable, user, host, version = get_standard_settings()
-        new_doc = "EDIT_ENABLED: true\nUSER: {0}\nHOST: {1}\nVERSION: {2}".format(user, host, version)
-        with open('resources/settings.txt', 'w') as text:
-            text.seek(0)
-            text.write(new_doc)
     except Exception as e:
         print("FATAL ERROR: Settings not writable.", e)
